26be4a4926c8e223eb64c85d45f26a227af1f39b
nguyenthanhdat2005tm2-eng <nguyenthanhdat2005.tm2@gmail.com>
2026-07-03 21:23:39 +0700

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <h2>About Paradise Nursery</h2>
+      <p>Welcome to Paradise Nursery, your number one source for all things plants. We're dedicated to providing you the very best of houseplants, with an emphasis on quality, variety, and customer service.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
